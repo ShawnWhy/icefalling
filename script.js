621b1166ed23a3527dab6f392c953fc2689a70a1
Shawn Yu <shawnyudesign@gmail.com>
2023-01-08 10:02:30 -0500

    sdsdsdsd

---
+++ script.js
@@ -0,0 +1,522 @@
+var characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','.',':','!','1','2','3','4','5','6','7','8','9','10'];
+var charactersColor = {}
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+for (i=0; i<characters.length; i++){
+  charactersColor[characters[i]] = getRandomColor()
+}
+
+var throttle = 'on'
+
+var blockBlock2=[]
+ capturedResponse=
+  {
+    "totalArticles": 297356,
+    "articles": [
+    {
+    "title": "Vekker oppsikt internasjonalt",
+    "description": "Norges protest blir lagt merke til.",
+    "content": "Gibraltar - Norge 0-3\nNorge protesterte mot VM i Qatar før kveldens seier mot Gibraltar.\nStåle Solbakken hadde på seg en T-skjorte hvor det sto «Respect on and off the pitch», mens spillerne brukte t-skjorter med «Human rights on and off the pitch» u... [578 chars]",
+    "url": "https://www.dagbladet.no/sport/vekker-oppsikt-internasjonalt/73570276",
+    "image": "https://www.dagbladet.no/images/73570596.jpg?imageId=73570596&panow=100&panoh=100&panox=0&panoy=0&heightw=100&heighth=100&heightx=0&heighty=0&width=1200&height=630",
+    "publishedAt": "2021-03-24T21:46:51Z",
+    "source": {
+    "name": "Dagbladet.no",
+    "url": "https://www.dagbladet.no"
+    }
+    },
+    {
+    "title": "Massive container ship blocking Suez Canal paralyses trade for second day",
+    "description": "The Ever Given was grounded early Tuesday amid poor visibility caused by a dust storm and wind speeds that reached 40 knots.. Read more at straitstimes.com.",
+    "content": "CAIRO (BLOOMBERG) - Tugs and diggers have so far failed to dislodge a massive container ship stuck in the Suez Canal on Wednesday (March 24), increasing the chances of prolonged delays in what is arguably the world’s most important waterway.\nWork to ... [5274 chars]",
+    "url": "https://www.straitstimes.com/world/suez-canal-stays-blocked-as-efforts-to-dislodge-stuck-container-ship-continues",
+    "image": "https://www.straitstimes.com/s3/files/styles/x_large/public/articles/2021/03/24/ak_eg3_2403.jpg?itok=byer4se2",
+    "publishedAt": "2021-03-24T21:42:43Z",
+    "source": {
+    "name": "The Straits Times",
+    "url": "https://www.straitstimes.com"
+    }
+    },
+    {
+    "title": "Momentum builds within Liberal party to consider quotas for women even as divisions emerge",
+    "description": "Defence industry minister Melissa Price says ‘the time has come’ for some kind of quota system to attract ‘qualified and motivated women’",
+    "content": "The federal social services minister, Anne Ruston, has pushed back against growing calls within her own party for the Liberals to adopt a quota system for women, saying politicians should not “dictate” decisions in a democratic party.\nBut the push fo... [5994 chars]",
+    "url": "https://www.theguardian.com/australia-news/2021/mar/25/momentum-builds-within-liberal-party-to-consider-quotas-for-women-even-as-divisions-emerge",
+    "image": "https://i.guim.co.uk/img/media/c0f355857d161df4f33a95e37e6ff66311c7fe9c/0_65_3320_1993/master/3320.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=a1a5792bbfa5168e120bc427ca891822",
+    "publishedAt": "2021-03-24T21:39:00Z",
+    "source": {
+    "name": "The Guardian",
+    "url": "https://www.theguardian.com"
+    }
+    },
+    {
+    "title": "Målløs Haaland da Norge slo miniputten Gibraltar",
+    "description": "Erling Braut Haaland måtte gå målløs av banen da Norge enkelt slo Gibraltar.",
+    "content": "Erling Braut Haaland måtte gå målløs av banen da Norge enkelt slo Gibraltar.\nGibraltar – Norge 0–3 (0–2)\nDet ble en frustrerende førsteomgang for landslaget, men like før pause løsnet det endelig.\nGibraltars keeper Dayle Coleing sto som en levende ve... [3793 chars]",
+    "url": "https://www.tv2.no/a/13908592/",
+    "image": "https://www.cdn.tv2.no/images/13908893.jpg?imageId=13908893&panow=100.00514208145&panoh=51.02813877551&panox=0.0051420814479575&panoy=17.006802721088&heightw=40.77740944206&heighth=100.0073316129&heightx=16.309012875536&heighty=0.0073316129032169&width=1200&height=630",
+    "publishedAt": "2021-03-24T21:37:39Z",
+    "source": {
+    "name": "TV 2",
+    "url": "https://www.tv2.no"
+    }
+    },
+    {
+    "title": "DOH wants vaccine line-jumpers punished; 5 mayors must explain",
+    "description": "The Department of Health (DOH) urged the public on Wednesday to watch closely the vaccination process to make sure that only people on the government’s priority list get shots against COVID-19 as the home affairs office ordered five mayors to explain why they were inoculated ahead of their turn to be vaccinated.",
+    "content": "MANILA, Philippines — The Department of Health (DOH) urged the public on Wednesday to watch closely the vaccination process to make sure that only people on the government’s priority list get shots against COVID-19 as the home affairs office ordered ... [7676 chars]",
+    "url": "https://newsinfo.inquirer.net/1411062/doh-wants-vaccine-line-jumpers-punished-5-mayors-must-explain",
+    "image": "https://newsinfo.inquirer.net/files/2021/03/News135352-620x351.jpg",
+    "publishedAt": "2021-03-24T21:34:00Z",
+    "source": {
+    "name": "INQUIRER.net",
+    "url": "https://newsinfo.inquirer.net"
+    }
+    },
+    {
+    "title": "Isro’s Chandrayaan-3 launch next year: Govt",
+    "description": "The space agency earlier planned to launch the Chandrayaan 3 mission either in late 2020 or early 2021. However, most missions were rescheduled due to the Covid-19 pandemic.",
+    "content": "The Indian Space Research Organisation (Isro) plans to launch its third mission to the Moon next year, the government said on Wednesday. In a written response to a question in the Lok Sabha, minister of state for the department of space, Jitendra Sin... [2821 chars]",
+    "url": "https://www.hindustantimes.com/india-news/isros-chandrayaan-3-launch-next-year-govt-101616619292766.html",
+    "image": "https://images.hindustantimes.com/img/2021/03/24/1600x900/_326c0358-2d6e-11eb-b9a2-d7de0b3760e9_1616621108852.png",
+    "publishedAt": "2021-03-24T21:26:40Z",
+    "source": {
+    "name": "Hindustan Times",
+    "url": "https://www.hindustantimes.com"
+    }
+    },
+    {
+    "title": "Hernando de Soto",
+    "description": "“He logrado hacerlo sin quitarle ninguna vacuna a mis compatriotas”, dijo el candidato presidencial de Avanza País a su llegada a Cusco la tarde de este miércoles.",
+    "content": "Conforme a los criterios de Saber más\nLuego de que las especulaciones crecieran en redes sociales, el candidato presidencial Hernando de Soto, de Avanza País, admitió este miércoles que se ha vacunado contra el COVID-19 durante sus recientes viajes a... [6917 chars]",
+    "url": "https://elcomercio.pe/politica/elecciones/elecciones-2021-hernando-de-soto-confirma-que-se-vacuno-contra-la-covid-19-en-estados-unidos-avanza-pais-covid-19-noticia/",
+    "image": "https://elcomercio.pe/resizer/XEOG-nQdIs57-Q79bfwZxC4UF24=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/2KRYGKM3JRCB3FWD3UYQ27NRKM.jpg",
+    "publishedAt": "2021-03-24T21:09:34Z",
+    "source": {
+    "name": "El Comercio Perú",
+    "url": "https://elcomercio.pe"
+    }
+    },
+    {
+    "title": "Jennifer Lopez og Alex Rodriguez:",
+    "description": "Selv om Jennifer Lopez og Alex Rodriguez ikke har endt forholdet, skal de likevel ha sine utfordringer.",
+    "content": "Tidligere i mars lyste skjermer verden over opp, da flere nettsteder kunne melde at Jennifer Lopez (51) og forloveden Alex Rodriguez (45) hadde brutt forlovelsen.\nDet viste seg imidlertid ikke å stemme, noe paret selv avkreftet.\n- Alle artiklene stem... [3713 chars]",
+    "url": "https://www.seher.no/kjendis/gjor-alt-de-kan/73570236",
+    "image": "https://www.seher.no/images/73570440.jpg?imageId=73570440&panow=100.00521284404&panoh=33.950166972477&panox=0.0052128440366909&panoy=5.5045871559633&heightw=100&heighth=100&heightx=0&heighty=0&width=1200&height=630",
+    "publishedAt": "2021-03-24T21:08:17Z",
+    "source": {
+    "name": "Seoghør.no",
+    "url": "https://www.seher.no"
+    }
+    },
+    {
+    "title": "Virginia Gov. Ralph Northam signs law abolishing death penalty",
+    "description": "After centuries of carrying out executions, Virginia on Wednesday became the 23rd state to abolish the death penalty after Gov. Ralph Northam signed historic legislation into law that ends capital punishment in the commonwealth.",
+    "content": "Gov. Ralph Northam walks past the holding cells next to the death chamber during a tour at the Greensville Correctional Center prior to signing a bill abolishing the state's death penalty in Jarratt, Virginia, on Wednesday, March 24.\n(CNN) —\nAfter ce... [5319 chars]",
+    "url": "https://www.cnn.com/2021/03/24/politics/virginia-governor-signs-law-death-penalty/index.html",
+    "image": "https://cdn.cnn.com/cnnnext/dam/assets/210324141323-01-northam-dealth-penalty-bill-0324-super-169.jpg",
+    "publishedAt": "2021-03-24T21:04:00Z",
+    "source": {
+    "name": "CNN",
+    "url": "https://www.cnn.com"
+    }
+    },
+    {
+    "title": "ההתרסקות של סער מוכיחה: יורשו של נתניהו יגיע מבית",
+    "description": "יו\"ר תקווה חדשה, שמאגפת את ראש הממשלה מימין, קיווה לסיים עם יותר משישה מנדטים. מהלך לאיחוד עם בנט היה מעלה את הסיכויים להוצאת נתניהו מבלפור, אך התכתשות ביניהם הביאה לאיבוד קולות. ולמרות הכשלון, ישנה דרך נוספת שיכולה לשנות דרמטית את התמונה",
+    "content": "רוב ראשי המפלגות ביקשו אמש (שלישי) להמתין לתוצאות הרשמיות בבחירות לכנסת ה-24, מרביתם גזרו על עצמם שתיקה זמנית אבל אחד מהם כבר הבין שהסיבוב הקרוב מבחינתו נגמר בהפסד. יו\"ר תקווה חדשה, גדעון סער, עמד אתמול בלילה על הבמה במטה המפלגה, והודה כי הייתה לו תק... [739 chars]",
+    "url": "https://elections.walla.co.il/item/3425707",
+    "image": "https://img.wcdn.co.il/f_auto,w_1200,t_54/ar_1.91,c_fill,g_auto/l_black_line,g_south,w_1.0,h_0.1,fl_relative,o_90/g_south_east,x_15,y_12,l_logo_2020,w_250/3/1/4/8/3148513-46.jpg",
+    "publishedAt": "2021-03-24T21:02:20Z",
+    "source": {
+    "name": "וואלה!",
+    "url": "https://elections.walla.co.il"
+    }
+    }
+    ]
+    }
+
+var blockBlock = []
+var sky= $(".sky")
+skyWidth = sky.width()
+skyHeight = sky.height()
+// console.log(skyHeight)
+// var skyWidth = sky.width;
+
+var openScreen= function(number){
+
+
+// console.log(blockBlock)
+setTimeout(() => {
+  $('.title').text(capturedResponse.articles[number].title)
+$('.information').text(capturedResponse.articles[number].content)
+$('.screen').removeClass('invisibleP')
+}, 400);
+
+
+setTimeout(() => {
+var windowWidth = $('body').width();
+var screenTop = $('.screen').offset().top;
+var screenLeft = $('.screen').offset().left;
+// console.log(screenLeft)
+// console.log(screenTop)
+var screenRight = parseInt(screenLeft)+parseInt(windowWidth*.8);
+// console.log(windowWidth);
+
+  blockBlock2=
+[{'height':parseInt(screenTop),
+'left':parseInt(screenLeft),
+'right':screenRight
+}]
+}, 1000);
+
+
+}
+
+$('.close').click((e)=>{
+e.preventDefault();
+e.stopPropagation();
+$('.screen').addClass('invisibleP')
+currentHeight = blockBlock2[0]['height']
+blockBlock2=[]
+
+var slidingIces = $('.slideIce')
+// console.log(slidingIces);
+// console.log(currentHeight)
+
+for(let i=0; i<slidingIces.length; i++){
+  // var iceheight = parseInt($(slidingIces[i]).css('top'))-10
+
+  // console.log(iceheight)
+
+  // if(iceheight==currentHeight){
+      $(slidingIces[i]).addClass('fallingIce')
+      $(slidingIces[i]).removeClass('slideIce')
+
+  // }
+
+}
+var slidingIces2 = $('.slideIce2')
+
+
+for(let i=0; i<slidingIces2.length; i++){
+
+
+ 
+
+  // if(iceheight==currentHeight){
+      $(slidingIces2[i]).addClass('fallingIce')
+      $(slidingIces2[i]).removeClass('slideIce2')
+
+  // }
+
+}
+
+var smallstill = $('.smallIceStill')
+
+for(let i=0; i<smallstill.length; i++){
+
+
+
+  // if(iceheight==currentHeight){
+      $(smallstill[i]).addClass('smallIce')
+      $(smallstill[i]).removeClass('smallIceStill')
+
+  // }
+
+}
+
+
+
+})
+
+
+var boxFall = function(){
+blockBlockUnite = blockBlock
+if(blockBlock2.length>0){
+blockBlockUnite = blockBlock.concat(blockBlock2)
+}
+   var smallIces = $('.smallIce')
+
+   for(let i=0; i<smallIces.length; i++){
+
+    var iceheight = parseInt($(smallIces[i]).css('top'))
+    var iceleft = parseInt($(smallIces[i]).css('left'))
+    iceheight+=5
+    $(smallIces[i]).css('top', iceheight)
+      for(let j=0; j<blockBlockUnite.length; j++){
+        if(iceheight>blockBlockUnite[j].height-5&&iceleft<blockBlockUnite[j].right&&iceleft>blockBlockUnite[j].left){
+          $(smallIces[i]).css('top',blockBlockUnite[j].height-5)
+            var innerht = $(smallIces[i]).html();
+           
+          $(smallIces[i]).removeClass('smallIce')
+          $(smallIces[i]).addClass('smallIceStill')
+     
+          }
+        }
+
+
+     if(iceheight>skyHeight-20){
+     $(smallIces[i]).remove()
+     }
+      }
+
+
+
+   
+
+  var fallingIces = $('.fallingIce')
+  // console.log(fallingIces)  
+  for(let i=0; i<fallingIces.length; i++){
+    var boxheight = parseInt($(fallingIces[i]).css('top'))
+    var boxleft = parseInt($(fallingIces[i]).css('left'))
+    boxheight+=15
+    // console.log(boxheight)
+    $(fallingIces[i]).css('top', boxheight)
+    // console.log($(fallingIces[i]).css('top'))
+      for(let j=0; j<blockBlockUnite.length; j++){
+        if(boxheight>blockBlockUnite[j].height-10&&boxleft<blockBlockUnite[j].right&&boxleft>blockBlockUnite[j].left){
+          $(fallingIces[i]).css('top',blockBlockUnite[j].height-10)
+            var innerht = $(fallingIces[i]).html();
+           if($(innerht).text()!==' '&& innerht!==''){
+          $(fallingIces[i]).removeClass('fallingIce')
+          $(fallingIces[i]).addClass('slideIce')
+          }
+          else{
+                   $(fallingIces[i]).removeClass('fallingIce')
+          $(fallingIces[i]).addClass('slideIce2')
+
+
+          }
+        }
+      }
+
+
+
+
+
+    
+    if(boxheight>skyHeight-20){
+      // console.log(throttle)
+      var newstripe= $("<div>")
+      newstripe.addClass('stripe')
+      newstripe.css('background-color', $(fallingIces[i]).css('background-color'))
+      if(throttle=='on'){
+      $(sky).prepend(newstripe)
+      throttle = 'off'
+      // console.log(throttle)
+      setTimeout(() => {
+        throttle='on'
+      }, 1500);
+      }
+      $(fallingIces[i]).remove()
+     }
+  }
+
+  
+
+    var slidingIces = $('.slideIce')
+   
+  for(let i=0; i<slidingIces.length; i++){
+    var slideheight = parseInt($(slidingIces[i]).css('top'))
+    var slideleft = parseInt($(slidingIces[i]).css('left'))
+    slideleft-=15
+    // console.log(boxheight)
+    $(slidingIces[i]).css('left', slideleft)
+    // console.log($(fallingIces[i]).css('top'))
+      for(let j=0; j<blockBlockUnite.length; j++){
+        if(slideheight==blockBlockUnite[j].height-10&&slideleft<blockBlockUnite[j].left+20){
+          // $(fallingIces[i]).css('top',blockBlock[j].height-10)
+           $(slidingIces[i]).addClass('fallingIce')
+           $(slidingIces[i]).removeClass('slideIce')
+         
+        }
+      }
+  }
+
+
+
+  
+    var slidingIces2 = $('.slideIce2')
+   
+  for(let i=0; i<slidingIces2.length; i++){
+    var slideheight = parseInt($(slidingIces2[i]).css('top'))
+    var slideleft = parseInt($(slidingIces2[i]).css('left'))
+    slideleft+=15
+    // console.log(boxheight)
+    $(slidingIces2[i]).css('left', slideleft)
+    // console.log($(fallingIces[i]).css('top'))
+      for(let j=0; j<blockBlockUnite.length; j++){
+        if(slideheight==blockBlockUnite[j].height-10&&slideleft>blockBlockUnite[j].right){
+          // $(fallingIces[i]).css('top',blockBlock[j].height-10)
+           $(slidingIces2[i]).addClass('fallingIce')
+           $(slidingIces2[i]).removeClass('slideIce2')
+        }
+      }
+  }
+  
+
+}
+
+
+var explodeBox = function(target){
+
+  // console.log("sdsdsdsdsd")
+var target = $(target)
+if(target.hasClass('slideIce')==false&&target.hasClass('slideIce2')==false&&target.hasClass('fallingIce')==false){
+  target=target.parent()
+}
+// console.log($(target).attr('number'));
+if($(target).attr('number')){
+
+openScreen($(target).attr('number'))
+}
+
+var targetLeft = target.css('left');
+var targetTop = target.css('top');
+
+for(i=0; i<5; i++){
+  let randDeg = Math.floor(Math.random()*100-50)
+  let randDeg2 = Math.floor(Math.random()*-100+50)
+  var smallBloc = $("<div>")
+
+    // smallBloc.addClass("fallingIce");
+    smallBloc.addClass("smallIce");
+    smallBloc.css("top", parseInt(targetTop)-randDeg)
+    smallBloc.css('left', parseInt(targetLeft)+randDeg2)
+    // smallBloc.css('transform', 'rotate('+ randDeg+ 'deg) translate(50px)')
+    
+    
+   
+      smallBloc.css('background-color',getRandomColor)
+      $(target).remove()
+      $('.sky').append(smallBloc)
+
+
+}    
+
+}
+
+
+$(document).on('mouseover','.slideIce2',function (e) { 
+  e.preventDefault()
+  e.stopPropagation()
+
+  explodeBox(e.target)
+  
+  
+});
+$(document).on('mouseover','.slideIce',function (e) { 
+
+  e.preventDefault()
+  e.stopPropagation()
+  explodeBox(e.target)
+  
+});
+
+$(document).on('mouseover','.fallingIce',function (e) { 
+
+   e.preventDefault()
+  e.stopPropagation()
+  explodeBox(e.target)
+  
+});
+
+setInterval(() => {
+  boxFall()
+}, 150);
+
+var dropBoxes = function(number){
+  let randomLeft = Math.random()*skyWidth
+  let title= capturedResponse.articles[number]['title']
+  // console.log(title); 
+  title = title.split('')
+  length= title.length
+  if(length>25){
+    length=25
+  }
+  for(i=0; i<length; i++){
+    let block=$("<div>");
+    block.addClass("fallingIce");
+    block.attr('number',number);
+    block.css("top", -10-(10*i))
+    block.css('left', randomLeft)
+    if(characters.includes( title[i])){
+    block.css('background-color',charactersColor[title[i]])
+    }
+    else{
+      block.css('background-color',getRandomColor)
+
+      }
+    
+    
+    block.html("<div class='innerBlock'>"+title[i]+"</div>")
+    $('.sky').append(block)
+  }
+
+
+
+}
+
+var initBlocks = function(){
+for (let i=0; i<24; i++){
+  // console.log(i)
+
+  createBlocks(
+    Math.floor(Math.random()*(skyHeight-300)+250),
+    Math.floor(Math.random()*skyWidth),
+    Math.floor(Math.random()*24+1),
+    getRandomColor()
+
+
+  )
+
+}
+
+}
+
+var createBlocks = function(height,left, width, color){
+
+var blockParent = $('<div>');
+blockParent.addClass('blockParent');
+blockParent.css('top', height)
+blockParent.css('left', left)
+for(let i=0; i<width; i++){
+  var block = $("<div>")
+  block.css("background-color",color)
+  blockParent.append(block);
+  var blockInfo = {
+    'height':height,
+    'left':left,
+    'right':left + 20*width
+  }
+  blockBlock.push(blockInfo)
+
+}
+$('.sky').append(blockParent)
+
+
+}
+
+  dropBoxes(
+    Math.floor(Math.random()*capturedResponse.articles.length)
+  )
+  
+
+setInterval(() => {
+  dropBoxes(
+    Math.floor(Math.random()*capturedResponse.articles.length)
+  )
+  
+}, 5000);
+initBlocks()
+
+
+
+
